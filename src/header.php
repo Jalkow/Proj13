3a46556c60802af7627793c58e3b0c37eb3214d3
Dorian Gehant <dgehant@gaming.tech>
2022-04-12 20:14:48 +0200

    debut header

---
+++ src/header.php
@@ -0,0 +1,18 @@
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg">
+        <div class="row col-12">
+            <a class="navbar-brand col-lg-2" href="#">
+                <img class="" src="img/logo.jpg" alt="logo alkya esport"></img>
+            </a>
+            <div class="collapse navbar-collapse col-2" id="navbarNavAltMarkup">
+                <a class="nav-link" href="#">Actus</a>
+                <a class="nav-link" href="#">About us</a>
+                <a class="nav-link" href="#">Equipes</a>
+                <a class="nav-link" href="#">Shop</a>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
+</div>
--- src/index.php
+++ src/index.php
@@ -8,9 +8,10 @@
         <link type="text/css" rel="stylesheet" href="css/style.css" media="screen,projection"/>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Alkya</title>
+        <title>Alkya - Structure esport</title>
     </head>
     <body>
+        <?php require "header.php";?>
 
         <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
             Link with href
@@ -20,6 +21,8 @@
                 Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
             </div>
         </div>
+
+        <?php require "footer.php";?>
 
         <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
         <script type="text/javascript" src="js/script.js"></script>
