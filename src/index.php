b78dc50f42d32422e0bf16978ff772eaa9f3cfe5
Dorian Gehant <dgehant@gaming.tech>
2022-04-12 12:02:21 +0200

    mise en place du projet

---
+++ src/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="css/style.css" media="screen,projection"/>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Alkya</title>
+    </head>
+    <body>
+
+        <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Link with href
+        </a>
+        <div class="collapse" id="collapseExample">
+            <div class="card card-body">
+                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+            </div>
+        </div>
+
+        <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
+    </body>
+</html>
